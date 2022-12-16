f4543a7d5b285e46c11fdf404fbbc96a530693cf
Luciano Marin <mescalkun@gmail.com>
2022-12-16 11:35:20 -0300

    Controlladores

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\controllerUsuario;
+use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+/*
+Route::post('/principal', [ControllerUsuario::class, 'store'])->name('usuario');
+*/
+
+/*
+Route::post('/principal', 'controllerUsuario@index');
+*/
